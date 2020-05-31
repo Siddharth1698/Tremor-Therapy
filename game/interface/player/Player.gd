d27d7f5807065ced0b68de1f19080ed379177872
Siddharth1698 <siddharth.m98@gmail.com>
2020-05-31 07:37:29 +0530

    adding movements to ball and fall off

--- game/interface/player/Player.gd
+++ game/interface/player/Player.gd
@@ -1,16 +1,32 @@
 extends KinematicBody2D
 
 var motion = Vector2(0,0)
-const SPEED = 500
+const SPEED = 1500
+const GRAVITY = 500
+const UP = Vector2(0,-1)
+const JUMP_SPEED = 3000
 
 
 func _physics_process(delta):
+	apply_gravity()
+	jump()
+	move()
+	move_and_slide(motion,UP)
+	
+func apply_gravity():
+	if is_on_floor():
+		motion.y = 0
+	else:
+		motion.y += GRAVITY
+	
+func move():
 	if Input.is_action_just_pressed("left") and not Input.is_action_pressed("right"):
 		motion.x = -SPEED
 	elif Input.is_action_just_pressed("right") and not Input.is_action_pressed("left"):
 		motion.x = SPEED
 	else:
 		motion.x=0
-	move_and_slide(motion)
-	
-	
+		
+func jump():
+	if Input.is_action_just_released("jump") and is_on_floor():
+		motion.y -= JUMP_SPEED
